a92892b284733a43018f36266e8f368cfbdaddd6
Hoang Phan <hoang.phan@sentifi.com>
2017-10-12 17:22:49 +0700

    feat(highlight): highlight odoo keyword as global object

--- src/main/kotlin/com/intellij/odoo/annotator/OdooAnnotator.kt
+++ src/main/kotlin/com/intellij/odoo/annotator/OdooAnnotator.kt
@@ -1,17 +1,33 @@
 package com.intellij.odoo.annotator
 
 import com.intellij.lang.annotation.*
+import com.intellij.lang.javascript.JSElementType
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiLiteralExpression
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 
 class OdooAnnotator : Annotator {
+
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element is PsiLiteralExpression) {
-            val value = if (element.value is String) element.value as String else null
-            if (value != null) {
-                println(value)
-            }
+        val psiElement = element as LeafPsiElement
+        val elementType = psiElement.elementType as? JSElementType ?: return
+        val text = psiElement.text
+        println(text)
+        if (text == GLOBAL_KEYWORD) {
+            val annotation = holder.createInfoAnnotation(element.textRange, null)
+            annotation.textAttributes = DefaultLanguageHighlighterColors.GLOBAL_VARIABLE
+            // get next element
         }
+        if (text == DEFINE_KEYWORD) {
+            // get next element
+            println(DEFINE_KEYWORD)
+        }
+    }
+
+    companion object {
+        val GLOBAL_KEYWORD = "odoo"
+        val DEFINE_KEYWORD = "define"
+        val MODULE_NAME = ""
     }
 
 }
