fdf574433cc2be498ca91e8c42d11add78dc707a
hoangpp <pquochoang2007@gmail.com>
2017-10-15 19:16:23 +0700

    remove unused lib

--- src/main/kotlin/com/intellij/odoo/annotator/OdooAnnotator.kt
+++ src/main/kotlin/com/intellij/odoo/annotator/OdooAnnotator.kt
@@ -2,12 +2,9 @@ package com.intellij.odoo.annotator
 
 import com.intellij.lang.annotation.*
 import com.intellij.lang.javascript.JSElementType
-import com.intellij.lang.javascript.psi.JSFunction
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.util.PsiUtil
 
 class OdooAnnotator : Annotator {
 
